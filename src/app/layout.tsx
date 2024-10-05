a4d4df6646d3ba4becf4355d09048fd9b196b820
rizki <rzkir571@gmail.com>
2024-10-06 03:09:57 +0700

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import "@/components/sass/globals.scss";
 
 import { Roboto } from '@next/font/google';
 
+import Script from "next/script";
+
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '700'],
@@ -42,6 +44,29 @@ export default function RootLayout({
             <Footer />
           </main>
         </Fragment>
+
+        <Script
+          id="google-tag-manager"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NSQXTQGF');
+          `,
+          }}
+        />
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NSQXTQGF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
