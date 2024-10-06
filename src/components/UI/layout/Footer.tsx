45c981814c3e53423e5e1408edb24ea24ae51439
rizki <rzkir571@gmail.com>
2024-10-07 03:14:38 +0700

    footer

--- src/components/UI/layout/Footer.tsx
+++ src/components/UI/layout/Footer.tsx
@@ -1,48 +1,12 @@
 import React from "react";
-
 import dynamic from "next/dynamic";
 
-const Profile = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Profile"),
-  {
-    ssr: false,
-  }
-);
-
-const Deskripsi = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Deskripsi"),
-  {
-    ssr: false,
-  }
-);
-
-const Social = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Social"),
-  {
-    ssr: false,
-  }
-);
-
-const Navigation = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Navigation"),
-  {
-    ssr: false,
-  }
-);
-
-const Documentation = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Documentation"),
-  {
-    ssr: false,
-  }
-);
-
-const Legal = dynamic(
-  () => import("@/components/UI/hooks/section/footer/Legal"),
-  {
-    ssr: false,
-  }
-);
+const Profile = dynamic(() => import("@/components/UI/hooks/section/footer/Profile"), { ssr: false });
+const Deskripsi = dynamic(() => import("@/components/UI/hooks/section/footer/Deskripsi"), { ssr: false });
+const Social = dynamic(() => import("@/components/UI/hooks/section/footer/Social"), { ssr: false });
+const Navigation = dynamic(() => import("@/components/UI/hooks/section/footer/Navigation"), { ssr: false });
+const Documentation = dynamic(() => import("@/components/UI/hooks/section/footer/Documentation"), { ssr: false });
+const Legal = dynamic(() => import("@/components/UI/hooks/section/footer/Legal"), { ssr: false });
 
 export default function Footer() {
   return (
@@ -52,27 +16,32 @@ export default function Footer() {
           <div className="profile">
             <Profile />
             <Deskripsi />
-
             <div className="social">
               <Social />
             </div>
           </div>
 
           <div className="footer__list">
-            <ul className="list">
+            <div>
               <h1>Navigation</h1>
-              <Navigation />
-            </ul>
+              <ul className="list">
+                <Navigation />
+              </ul>
+            </div>
 
-            <ul className="list">
+            <div>
               <h1>Documentation</h1>
-              <Documentation />
-            </ul>
+              <ul className="list">
+                <Documentation />
+              </ul>
+            </div>
 
-            <ul className="list">
+            <div>
               <h1>Legal</h1>
-              <Legal />
-            </ul>
+              <ul className="list">
+                <Legal />
+              </ul>
+            </div>
           </div>
         </div>
 
