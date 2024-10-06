be7338f9bbb2630f51512df5195800ea31f32e23
rizki <rzkir571@gmail.com>
2024-10-07 01:40:14 +0700

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -32,6 +32,16 @@ const nextConfig = {
       enable: true,
     },
   },
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_API_TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
+    NEXT_PUBLIC_API_URL_MARQUE: process.env.NEXT_PUBLIC_API_URL_MARQUE,
+    NEXT_PUBLIC_API_URL_PROYEK: process.env.NEXT_PUBLIC_API_URL_PROYEK,
+    NEXT_PUBLIC_API_URL_BLOG: process.env.NEXT_PUBLIC_API_URL_BLOG,
+    NEXT_PUBLIC_API_URL_PORTOFOLIO: process.env.NEXT_PUBLIC_API_URL_PORTOFOLIO,
+    NEXT_PUBLIC_API_APP_SCRIPT: process.env.NEXT_PUBLIC_API_APP_SCRIPT,
+    NEXT_PUBLIC_SITE_VERTIFICATION: process.env.NEXT_PUBLIC_SITE_VERTIFICATION,
+  },
 };
 
 import withBundleAnalyzer from "@next/bundle-analyzer";
